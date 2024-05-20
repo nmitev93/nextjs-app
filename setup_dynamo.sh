5d2038f917b03a6601fc7a37f326d372c1aa834f
Nikola Mitev <nikola.mitev.93@hotmail.com>
2024-05-20 15:18:43 +0300

    setup simple DynamoDB storage.

---
+++ setup_dynamo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ENDPOINT="http://localhost:8000"
+TABLE_NAME="nextjs-app"
+
+echo "List of current tables in DynamoDB."
+aws dynamodb list-tables \
+  --endpoint-url ${ENDPOINT}
+
+echo "Delete ${TABLE_NAME} if exists."
+aws dynamodb delete-table \
+  --endpoint-url ${ENDPOINT} \
+  --table-name ${TABLE_NAME} \
+  --no-cli-pager
+
+echo "Creating DynamoDB table for the app."
+aws dynamodb create-table \
+  --endpoint-url ${ENDPOINT} \
+  --table-name ${TABLE_NAME} \
+  --attribute-definitions '[{"AttributeName": "primary_key","AttributeType": "S"}, {"AttributeName": "secondary_key","AttributeType": "S"}]' \
+  --key-schema '[{"AttributeName": "primary_key","KeyType": "HASH"}, {"AttributeName": "secondary_key","KeyType":"RANGE"}]' \
+  --billing-mode PAY_PER_REQUEST \
+  --no-cli-pager
